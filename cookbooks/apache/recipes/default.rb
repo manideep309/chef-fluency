c80cda47292e534603f201b86209de3069216fb9
manideep <manideep.uf165@gmail.com>
2017-11-27 20:20:15 +0000

    new version updated

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,11 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+if node['platform_family'] == "rhel"
+       package = "httpd"
+elsif node['platform_family'] == "debian"
+       package "apache2"
+end 
+
 package 'apache2' do
        package_name 'httpd' 
+       action :install
 end
 
-service 'apach2' do
+service 'apache2' do
          service_name 'httpd'
-         action[:start, :enable]
+         action [:start, :enable]
 end
+
+include_recipe 'apache::websites'
